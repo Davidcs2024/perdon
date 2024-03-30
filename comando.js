9b37c51e77abea1d5b264404ab102b8ddc5a77c8
Davidcs2024 <165453550+Davidcs2024@users.noreply.github.com>
2024-03-30 00:05:32 -0600

    Update comando 2.js

--- comando.js
+++ comando.js
@@ -1,13 +1,13 @@
 const yesBtn = document.querySelector('#yesBtn');
 
-yesBtn.addEventListener('click',function () {
+yesBtn.getElementById('click',function () {
     alert('SOY UN GRAN IDIOTA, PERDONAME')
     alert('¿PODEMOS PLATICAR EN PERSONA?')
 });
 
 const noBtn = document.querySelector('#noBtn');
 
-noBtn.addEventListener('mouseover', function () {
+noBtn.getElementById('mouseover', function () {
     const randomX = parseInt(Math.random()*100);
     const randomY = parseInt(Math.random()*100);
     noBtn.style.setProperty('top',randomY+'%');
