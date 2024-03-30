496d07e1a66d33510aebb8b5524d62687bd52dcf
Davidcs2024 <165453550+Davidcs2024@users.noreply.github.com>
2024-03-30 00:15:18 -0600

    Update comando 3.js

--- comando.js
+++ comando.js
@@ -1,16 +1,23 @@
-const yesBtn = document.querySelector('#yesBtn');
+const yesBtn = document.getElementById('#yesBtn');
 
-yesBtn.getElementById('click',function () {
+yesBtn.addEventListener('click', function () {
     alert('SOY UN GRAN IDIOTA, PERDONAME')
     alert('¿PODEMOS PLATICAR EN PERSONA?')
 });
 
-const noBtn = document.querySelector('#noBtn');
+const noBtn = document.getElementById('#noBtn');
 
-noBtn.getElementById('mouseover', function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    noBtn.style.setProperty('top',randomY+'%');
-    noBtn.style.setProperty('left',randomX+'%');
-    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
-})
+function moveButton() {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+    noBtn.style.setProperty('transform', `translate(-${randomX}%, -${randomY}%)`);
+}
+
+noBtn.addEventListener('mouseover', moveButton);
+
+noBtn.addEventListener('touchmove', function (e) {
+    e.preventDefault(); 
+    moveButton();
+});
